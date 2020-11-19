f844be4562d1bfae7eb257f063275f9c1f8d4252
KenHobi <74683854+KenHobi@users.noreply.github.com>
2020-11-19 15:51:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!-- saved from url=(0019)http://lolicops.tk/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>내 사랑</title>
+    <link href="./volta_files/index.css" rel="stylesheet" type="text/css">
+	<link rel="icon" href="./volta_files/favicon.jpg">
+</head>
+<body>
+<div class="container"> 
+<header class="header">
+  
+</header>
+<center><h1 class="logo">oǝ̗ɯoᴚ<br> </h1>
+  <h1 class="logo2">𝐸𝒽 𝒷𝒾𝑒𝓃, 𝒿𝑒 𝓉'𝒶𝒾𝓂𝑒.<br><br>
+
+𝑀𝑜𝓃 𝑅𝑜𝒾, 𝒿𝑒 𝓉'𝒶𝒾𝓂𝑒. 𝒩𝑜𝓊𝓈 𝒹𝑒𝓊𝓍 𝒸'𝑒𝓈𝓉 𝓁'𝒶𝓂𝑜𝓊𝓇 𝒻𝑜𝓊𝓌.
+    𝒪𝓃 𝑒𝓈𝓉 𝓊𝓃 𝓅𝑒𝓊 𝒲𝑒𝒾𝓇𝒹𝑜𝓌, 𝑜𝓃 𝒶𝒾𝓂𝑒 𝑀𝒶𝓃𝑔𝑒𝓌, 𝒻𝒶𝒾𝓇𝑒 𝒹𝑒𝓈 𝒞â𝓁𝒾𝓃𝑜𝓊𝓊𝓊𝓊𝓊. 𝑀𝒶 𝑀𝒶𝓇𝓂𝑜𝓉𝓉𝑒, 𝓂𝑜𝓃 𝒫𝒽𝑜𝓉𝑜𝑔𝓇𝒶𝓅𝒽𝑒 𝒻𝒶𝓋𝑜𝓇𝒾𝓈, 𝐿𝑒 𝐵𝑒𝒢𝑒𝒲, 𝑀𝒪𝒩 𝐵𝐸𝒢𝐸𝒲,<br><br>
+
+    𝓂𝑜𝓃 𝐵𝒶𝒷𝓎. 𝒥𝐸 𝒯'𝒜𝐼𝑀𝐸 :𝟥</h1></center>
+<div class="gallery">
+<div class="thumbnail"> <img src="./volta_files/musics.jpg" alt="" width="1440" class="gallery"
+     onclick="window.location=&#39;Mangew.html&#39;">
+  <h4>Mangew</h4>
+  <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/anime.jpg" alt="" width="1440" class="gallery"
+     onclick="window.location=&#39;marmotte.html&#39;">
+      <h4>Marmotte</h4>
+      <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/manga.jpg" alt="" width="1440" class="gallery" onclick="window.location=&#39;mon roi.html&#39;">
+      <h4>Mon roi</h4>
+      <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/films.jpg" alt="" width="1440" class="gallery" onclick="window.location=&#39;photographe.html&#39;">
+      <h4>Photographe</h4>
+      <p class="tag"></p>
+</div>
+  </div>
+  <div class="gallery">
+    <div class="thumbnail"> <img src="./volta_files/flap.jpg" alt="" width="1440" class="gallery"
+     onclick="window.location=&#39;calinou.html&#39;">
+      <h4>Câlinouuuu </h4>
+      <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/seriesTV.jpg" alt="" width="1440" class="gallery"
+     onclick="window.location=&#39;weirdow.html&#39;">
+      <h4>Weirdow </h4>
+      <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/chepa.jpg" alt="" width="1440" class="gallery"
+		onclick="window.location=&#39;aide.html&#39;">
+      <h4>Le BeGew</h4>
+      <p class="tag"></p>
+</div>
+    <div class="thumbnail"> <img src="./volta_files/images.jpg" alt="" width="1440" class="gallery"
+     onclick="window.location=&#39;nous.html&#39;">
+      <h4>Nous :3</h4>
+      <p class="tag"></p>
+</div>
+  </div>  
+<footer id="contact">
+  <p class="hero_header">Il faut que tu trouves la solution de la jolie énigme que voici.<br>
+Creuses toi les méninges, krkrk</p>
+  <div class="button" onclick="passWord()"><strong>∩( ・ω・)∩</strong><br>
+
+<script>
+function passWord() {
+var testV = 1;
+var pass1 = prompt("Trouveras-tu le code secret qui te permettra d'obtenir un cholikado !","(っ° 0 °)っ Toujours de la manière la plus épurée, sans accents ni majuscules");
+while (testV < 3) {
+if (!pass1) history.go(-1);	
+if (pass1.toLowerCase() == "romeo tu es toute ma vie ily amios") {
+alert("Chapeau mon Ange, tu as tout réussi, je suis content d'avoir fait ça, c'était compliqué et sympathique de se creuser les méninges, merci d'être là pour moi. Merci beaucoup, pour tout ! Je t'aime. Ehp ehp ehp ton pti casdeau, l'oublies pas ! ");
+break;}
+testV+=1;
+var pass1 = prompt('Tu y es presque mouhahaha, réfléchis encore un peu :p','');}
+if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);}
+</script>
+
+</div>
+</footer>  
+<div class="copyright" ><strong>(っ◔◡◔)っ 𝓹𝓪𝔀𝓬𝓮 𝓺𝓾𝓮 𝓳𝓮 𝓽'𝓮𝔀𝓶</strong><br>
+</div>
+
+</body></html>
